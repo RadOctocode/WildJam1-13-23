9e856eeacfd888dea07149b2eedf38c37f93e17f
Taylor Foxhall <lxmachina@gmail.com>
2023-01-15 16:53:35 -0500

    Queue animals for spawning

--- Script/Spawner.cs
+++ Script/Spawner.cs
@@ -4,23 +4,51 @@ using System.Collections.Generic;
 
 public class Spawner : Node
 {
-	private bool canSpawn = false;
-
 	// List of animals to pull from when spawning.
 	[Export]
 	public List<PackedScene> Animals;
 	// Camera used by the game to look at the tower.
 	public Position3D GameCamera;
+	
+	private bool canSpawn = false;
+	private Queue<int> queue;
+	private const int QUEUE_SIZE = 3;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		initQueue();
 		canSpawn = true;
 	}
+	
+	private void initQueue() {
+		queue = new Queue<int>();
+		for (int i = 0; i < QUEUE_SIZE; ++i) {
+			enqueueAnimal();
+		}
+	}
+	
+	/** Randomly select an index for an animal from the possible set of initialized animals.
+	* This index is invalid if `Animal` mutates after being called.
+	*/
+	private int chooseAnimal() {
+		return (int)(GD.Randi() % Animals.Count);
+	}
+	
+	private void enqueueAnimal() {
+		int next = chooseAnimal();
+		GD.Print("Enqueing animal ", next);
+		queue.Enqueue(next);
+	}
+	
+	private int rotateQueue() {
+		int index = queue.Dequeue();
+		enqueueAnimal();
+		return index;
+	}
 
-	public void SpawnAnimal(Vector2 pos) {
-		var choice = GD.Randi() % Animals.Count;
-		var animal = (Spatial)Animals[(int)choice].Instance();
+	public void SpawnAnimal(PackedScene animalScene, Vector2 pos) {
+		var animal = (Spatial)animalScene.Instance();
 		var distanceToOrigin = GameCamera.Translation.z;
 		var spawnPoint = GameCamera.GetNode<Camera>("Camera").ProjectPosition(pos, distanceToOrigin);
 		animal.Translation = spawnPoint;
@@ -35,7 +63,8 @@ public class Spawner : Node
 			&& @event is InputEventMouseButton eventMouseButton
 			&& eventMouseButton.Pressed)
 		{
-			SpawnAnimal(eventMouseButton.Position);
+			PackedScene animal = Animals[rotateQueue()];
+			SpawnAnimal(animal, eventMouseButton.Position);
 		}
 	}
 
