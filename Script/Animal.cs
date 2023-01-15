57c7e425855ece4184ce15e5e5ebaec473699685
Michelle Tan <mtan2198@gmail.com>
2023-01-15 16:58:28 -0500

    commit

--- Script/Animal.cs
+++ Script/Animal.cs
@@ -1,20 +1,22 @@
 using Godot;
 using System;
 
-public class Animal : Node
+public class Animal : Spatial
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
 
-	// Called when the node enters the scene tree for the first time.
+
 	public override void _Ready(){
-		
+		GetNode<Timer>("AnimalTimer").Start();
 	}
-
+	
+	public void _on_Timer_timeout()
+	{
+		body.queue_free();
+	}
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
 //  {
 //      
 //  }
 }
+
